70714cf56f4b36c14f15f02d06f927717482a9bb
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-11 18:27:46 +0500

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name])
+
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+      flash[:success] = 'You successfuly logged in'
+    else
+      render :new
+      flash[:alert] = 'Name is invalid'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+    flash[:success] = 'You successfuly logged out'
+  end
+end
