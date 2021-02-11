8cd2dfdde1a8ec10c8cb9e0d1f701f1d3cc03ecd
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-11 18:24:56 +0500

    change application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    
+    private
+
+    def logged_in
+        unless current_user
+            flash[:notice] = "Sign up or Sign in to see events"
+            redirect_to login_path            
+        end
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
 end
