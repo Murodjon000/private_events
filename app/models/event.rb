a83fa51028554fbcd325c070bfe337a7cf3d5300
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-11 15:11:46 +0500

    add event model

---
+++ app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ApplicationRecord
+    belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+    has_many :enrollments
+    has_many :attendees, through: :enrollments, source: :user
+    scope :past, -> { where('date < ?', Date.today) }
+    scope :upcoming, -> { where('date >= ?', Date.today) }
+end
