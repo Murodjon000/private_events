97a1e54d050605d0b147551167569d0d7db219c9
Ijay Abby <inyangasi4@gmail.com>
2021-02-11 17:51:08 +0300

    add methods in evens controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,33 @@
 class EventsController < ApplicationController
-  def new
+
+  before_action :logged_in, except: [:index] 
+
+  def index
+    @events = Event.all
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)    
+    if @event.save
+      flash[:success] = "Event '#{@event.name}' successfuly created."
+      redirect_to @event
+    else
+      flash[:alert] = "Error happened."
+      render :new
+    end
+  end 
+
   def show
+    @event = Event.find(params[:id])
+  end  
+
+  private 
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location, :user_id)
   end
 end
