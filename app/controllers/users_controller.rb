26773dccfe613c8a8bac34d8205b2f4cbda97875
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-11 18:25:15 +0500

    change users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
-  
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'You successfuly signed up'
+      redirect_to @user
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
