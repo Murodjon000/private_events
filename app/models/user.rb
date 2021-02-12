f97725fc6f15cf3cd3f0b7302eefab823537a592
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-12 19:34:23 +0500

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :events, inverse_of: :creator
     has_many :enrollments
     has_many :attended_events, through: :enrollments, source: :event
+    validates :name, presence: true
 end
