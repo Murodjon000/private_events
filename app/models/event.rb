cdccc952af0e81bf85bb5281a825eb45bdd355f0
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-12 19:34:08 +0500

    fix event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+    validates :name, :description, :date, :location, presence: true
     has_many :enrollments
     has_many :attendees, through: :enrollments, source: :user
     scope :past, -> { where('date < ?', Date.today) }
