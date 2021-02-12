ccf11b2d2b486fa92614daf094e00ac4ff45e51a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-12 19:33:52 +0500

    fix sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_name(params[:name])
+    user = User.find_by(name: params[:name])
 
     if user
       session[:user_id] = user.id
