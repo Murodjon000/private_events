9a0d89095ffb0b658c8938256e92a79ca17561f6
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-13 20:49:55 +0500

    add spec models user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { build(:user) }
+
+  describe 'associations' do
+    it { is_expected.to have_many(:events) }
+
+    it { is_expected.to have_many(:attended_events).through(:enrollments).source(:event) }
+  end
+  
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+end
