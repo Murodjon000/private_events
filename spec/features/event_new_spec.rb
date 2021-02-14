f3fb78b7e4904e9ffac5d8306a0d8d991e30d9b6
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-14 15:19:40 +0500

    add event new spec

---
+++ spec/features/event_new_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Eventnew', type: :feature do
+    let (:user) { create(:user) }
+
+    before do
+        feature_sign_in(user)
+        visit new_event_path
+        fill_in 'Name', with: 'Cocke Part'
+        fill_in 'Description', with: 'Hello Guys of Uzbekistan.'
+        click_button 'Create new event'
+    end
+
+    it 'should redirect to event page after creating' do
+        expect(page).to have_current_path(event_path(Event.first.id))
+    end
+
+    it 'should have success message after creating' do
+        expect(page).to have_text('Event successfuly created')
+    end
+end
