0b2ed4d099514d345337a6de70a9e6bc1cff31e6
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-15 18:42:39 +0500

    fix indentation in spec features event_index

--- spec/features/event_index_spec.rb
+++ spec/features/event_index_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'EventIndices', type: :feature do
-    it 'should list all upcoming events' do
-        event = create(:event, :upcoming)
-        enrollment = create(:enrollment, event: event)
-        user = enrollment.user
- 
-        feature_sign_in(user)
-        visit events_path
-    
-        expect(page).to have_current_path(events_path)
-    end
+  it 'should list all upcoming events' do
+    event = create(:event, :upcoming)
+    enrollment = create(:enrollment, event: event)
+    user = enrollment.user
 
-    it 'should list all past events' do
-        event = create(:event, :past)
-        enrollment = create(:enrollment, event: event)
-        user = enrollment.user
- 
-        feature_sign_in(user)
-        visit events_path
-    
-        expect(page).to have_current_path(events_path)
-    end
+    feature_sign_in(user)
+    visit events_path
+
+    expect(page).to have_current_path(events_path)
+  end
+
+  it 'should list all past events' do
+    event = create(:event, :past)
+    enrollment = create(:enrollment, event: event)
+    user = enrollment.user
+
+    feature_sign_in(user)
+    visit events_path
+
+    expect(page).to have_current_path(events_path)
+  end
 end
