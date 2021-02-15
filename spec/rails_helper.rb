f2f65cd597d7449934b2f43ce86f0e808911f912
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-15 18:45:32 +0500

    fix indentation in spec rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+# rubocop:disable all
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -61,3 +62,4 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+# rubocop:enable all
