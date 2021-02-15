152ec8f9140a3ecafe7da539201fb93692214737
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-15 18:44:01 +0500

    fix indentation in spec features users_show

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,36 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Usershow page', type: :feature do
-    it 'should list all user events' do
-       enrollment = create(:enrollment)
-       user = enrollment.user
-       event = enrollment.event
+  it 'should list all user events' do
+    enrollment = create(:enrollment)
+    user = enrollment.user
+    event = enrollment.event
+    feature_sign_in(user)
+    visit user_path(user)
 
-       feature_sign_in(user)
-       visit user_path(user)
-   
-       expect(page).to have_link(event.name, href: event_path(event))
-    end
+    expect(page).to have_link(event.name, href: event_path(event))
+  end
 
-    it 'should list all upcoming events' do
-        event = create(:event, :upcoming)
-        enrollment = create(:enrollment, event: event)
-        user = enrollment.user
- 
-        feature_sign_in(user)
-        visit user_path(user)
-    
-        expect(page).to have_link(event.name, href: event_path(event))
-    end
+  it 'should list all upcoming events' do
+    event = create(:event, :upcoming)
+    enrollment = create(:enrollment, event: event)
+    user = enrollment.user
 
-    it 'should list all past events' do
-        event = create(:event, :past)
-        enrollment = create(:enrollment, event: event)
-        user = enrollment.user
- 
-        feature_sign_in(user)
-        visit user_path(user)
-    
-        expect(page).to have_link(event.name, href: event_path(event))
-    end
+    feature_sign_in(user)
+    visit user_path(user)
+
+    expect(page).to have_link(event.name, href: event_path(event))
+  end
+
+  it 'should list all past events' do
+    event = create(:event, :past)
+    enrollment = create(:enrollment, event: event)
+    user = enrollment.user
+
+    feature_sign_in(user)
+    visit user_path(user)
+
+    expect(page).to have_link(event.name, href: event_path(event))
+  end
 end
