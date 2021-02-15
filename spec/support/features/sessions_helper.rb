931fe86f305c0b50efaeffb44c22c96ef626aa30
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-15 18:46:05 +0500

    fix indentation in spec sessions helper

--- spec/support/features/sessions_helper.rb
+++ spec/support/features/sessions_helper.rb
@@ -1,10 +1,10 @@
 module Features
-    module SessionsHelper
-      def feature_sign_in(user = nil)
-        user ||= create(:user)
-        visit new_session_path
-        fill_in 'Name', with: user.name
-        click_button 'Log in'
-      end
+  module SessionsHelper
+    def feature_sign_in(user = nil)
+      user ||= create(:user)
+      visit new_session_path
+      fill_in 'Name', with: user.name
+      click_button 'Log in'
     end
+  end
 end
