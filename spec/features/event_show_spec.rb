8b5983349727e95615f193f4bf01e75e188b0ab0
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-15 18:43:15 +0500

    fix indentation in spec features event_show

--- spec/features/event_show_spec.rb
+++ spec/features/event_show_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Eventshow', type: :feature do
-    let (:event) { create(:event) }
-    let (:user) { create(:user) }
+  let (:event) { create(:event) }
+  let (:user) { create(:attendee) }
+  let (:enrollment) { create(:enrollment, user: user, event: event) }
 
-    before do
-        feature_sign_in(user)
-        visit event_path(event)
-    end
+  before do
+    feature_sign_in(user)
+    visit event_path(event)
+  end
 
-    it 'should show the event details' do
-        expect(page).to have_text(event.name)
-    end
+  it 'should show the event name' do
+    expect(page).to have_text(event.name)
+  end
+
+  it 'should show the event date' do
+    expect(page).to have_text(event.date)
+  end
+
+  it 'should show the event description' do
+    expect(page).to have_text(event.description)
+  end
 end
